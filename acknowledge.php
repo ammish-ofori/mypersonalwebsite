eebac812d934a3e22f384a48d2a33630229322aa
kelechiudoagwu <kelechi.udoagwu@gmail.com>
2015-07-18 13:41:15 +0000

    php form trial5

---
+++ acknowledge.php
@@ -0,0 +1,12 @@
+<?php
+if (isset($_POST['send'])) {
+    
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<h1>Thank You</h1>
+<p>Your message has been sent.</p>
+<h1>Oops!</h1>
+<p>Sorry, there was a problem sending your message.</p>
+</html>
