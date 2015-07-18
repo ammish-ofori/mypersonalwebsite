e1b9c87c242ee31c0bf3eacd354a064d16522627
kelechiudoagwu <kelechi.udoagwu@gmail.com>
2015-07-18 14:11:03 +0000

    php form trial6

--- acknowledge.php
+++ acknowledge.php
@@ -1,12 +1,25 @@
 <?php
 if (isset($_POST['send'])) {
-    
+    $to = 'kelechiudoagwu@gmail.com'; // Use your own email address
+    $subject = 'Potential client from my site'; 
+    $message = 'Name: ' . $_POST['name'] . "\r\n\r\n";
+    $message .= 'Email: ' . $_POST['email'] . "\r\n\r\n";
+				$message .= 'Inquiry: ' . $_POST['inquiry'];
+				$success = mail($to, $subject, $message);
 }
 ?>
 <!DOCTYPE html>
-<html lang="en">
-<h1>Thank You</h1>
-<p>Your message has been sent.</p>
-<h1>Oops!</h1>
-<p>Sorry, there was a problem sending your message.</p>
+	<html lang="en">
+	<head>
+		<title>Message Confirmation</title>
+	</head>
+	<body>
+	<?php if (isset($success) && $success) { ?>
+	<h1>Thank You</h1>
+	<p>Your message has been sent.</p>
+	<?php } else { ?>
+	<h1>Oops!</h1>
+	<p>Sorry, there was a problem sending your message.</p>
+<?php } ?>
+</body>
 </html>
